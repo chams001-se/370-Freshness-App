5c1fe64cfa56bee04ec1667f9bf70736fbcf2877
no-one-pixel <kgbillan@gmail.com>
2025-11-17 21:23:00 -0800

    -UI Rework -Added settings button -Refactoring

--- code/src/FoodExpirationPanel.java
+++ code/src/FoodExpirationPanel.java
@@ -1,11 +1,24 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Line2D;
 
 // TODO
 public class FoodExpirationPanel extends JPanel{
     FoodExpirationPanel(){
         setLayout(new BorderLayout());
         JButton the = new JButton("the");
-        this.add(the, BorderLayout.NORTH);
+        //this.add(the, BorderLayout.NORTH);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        // It is necessary to take the parameter of the overridden paintComponent and
+        // cast it into a Graphics2D as it is required in order to be able to draw anything.
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setStroke(new BasicStroke(10f));
+        g2.draw(new Line2D.Double(0,0,500,0));
+
     }
 }
